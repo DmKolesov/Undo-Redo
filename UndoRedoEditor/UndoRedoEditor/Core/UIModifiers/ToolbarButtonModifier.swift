d158092d6dd56137c01f41da5e562fabf119951b
Dima Kolesov <kolesov.public@gmail.com>
2025-05-12 15:55:06 +0300

    add ToolbarButtonModifier

---
+++ UndoRedoEditor/UndoRedoEditor/Core/UIModifiers/ToolbarButtonModifier.swift
@@ -0,0 +1,27 @@
+//
+//  ToolbarButtonModifier.swift
+//  UndoRedoEditor
+//
+//  Created by Dima Kolesov on 12.05.2025.
+//
+
+import Foundation
+import SwiftUI
+
+struct ToolbarButtonModifier: ButtonStyle {
+    let isActive: Bool
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.system(size: 20, weight: .medium))
+            .padding(5)
+            .background(
+                Circle()
+                    .fill(isActive ? Color.accentColor.opacity(0.2) : Color.clear)
+            )
+            .foregroundColor(isActive ? Color.accentColor : Color.primary)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+            .animation(.interactiveSpring(), value: configuration.isPressed)
+    }
+}
+
